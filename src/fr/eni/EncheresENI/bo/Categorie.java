c52e454c6da842044161cc470400fdb4cd678416
Anthony Fernandez <anthony.fernandez2021@campus-eni.fr>
2021-07-06 09:50:51 +0200

    Fix BO

---
+++ src/fr/eni/EncheresENI/bo/Categorie.java
@@ -0,0 +1,39 @@
+package fr.eni.EncheresENI.bo;
+
+import java.util.List;
+
+public class Categorie {
+	private Integer noCategorie;
+	private String libelle;
+	private List<ArticleVendu> lstArticles; 
+	
+	public Categorie(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public Categorie(Integer noCategorie, String libelle) {
+		this.noCategorie = noCategorie;
+		this.libelle = libelle;
+	}
+
+	public Integer getNoCategorie() {
+		return noCategorie;
+	}
+
+	public void setNoCategorie(Integer noCategorie) {
+		this.noCategorie = noCategorie;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	@Override
+	public String toString() {
+		return "Categorie [noCategorie=" + noCategorie + ", libelle=" + libelle + ", lstArticles=" + lstArticles + "]";
+	}
+}
